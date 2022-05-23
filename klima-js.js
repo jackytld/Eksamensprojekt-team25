cc9d92884f40d1313f4bfbfe4246e9a7685caf5b
jackytld <95678364+jackytld@users.noreply.github.com>
2022-05-23 17:16:00 +0200

    js

---
+++ klima-js.js
@@ -0,0 +1,21 @@
+const carouselSlide = document.querySelector('.slider-container');
+const carouselImages = document.querySelectorAll('.slider-container img');
+
+
+//Knapper
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener('click', function() {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
