0292b0d8ad752059938b4a03dcc8da4bf011aa79
jackytld <95678364+jackytld@users.noreply.github.com>
2022-05-23 17:50:23 +0200

    pretty much done

--- klima-js.js
+++ klima-js.js
@@ -19,3 +19,23 @@ nextBtn.addEventListener('click', function() {
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
+
+prevBtn.addEventListener('click', function() {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', function() {
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -2 ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
